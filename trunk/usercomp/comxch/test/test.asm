db454008b33ee4394df65659361ba267b45f653c
thvortex <thvortex@52a0f2d4-074f-11de-b753-7319d013a8fa>
2009-03-06 02:35:16 +0000

    Initial checkin of all sources from the website.

---
+++ trunk/usercomp/comxch/test/test.asm
@@ -0,0 +1,12 @@
+; ******************************************************
+; BASIC .ASM template file for AVR
+; ******************************************************
+
+.include "C:\VMLAB\include\m8def.inc"
+
+.org 0
+begin:
+	rjmp loop
+loop:
+	rjmp loop
+
