848e1f71fd87b69d19282b66f148484334d05c13
thvortex <thvortex@52a0f2d4-074f-11de-b753-7319d013a8fa>
2009-08-07 21:16:15 +0000

    Initial support for extreme OCR values in PWM PC mode

--- trunk/useravr/useravr.h
+++ trunk/useravr/useravr.h
@@ -1,38 +1,20 @@
 #include <stdio.h>
+#include <stdarg.h>
 
-// TODO: Need new warning categories for port conflicts
-
-// Wrapper around TAKEOVER_PORT() function to provide common error handling. If an error occurs, the
-// port name is embedded into the warning message.
-#define TAKEOVER_PORT(pPort, pTakeIt, pForceOutput) { \
-      int rc = TAKEOVER_PORT(pPort, pTakeIt, pForceOutput); \
-      if (rc == PORT_NOT_OUTPUT) { \
-         WARNING("Pin "#pPort" is not defined as output", CAT_TIMER, WARN_TIMERS_OUTPUT); \
-      } else if (rc == PORT_NOT_OWNER) { \
-         WARNING("Pin "#pPort" already in use by another peripheral", CAT_TIMER, WARN_TIMERS_OUTPUT); \
-      } \
-   }
-
-// Wrapper around SET_PORT() function to provide common error handling. If an error occurs, the
-// port name is embedded into the warning message.
-#define SET_PORT(pPort, pValue) { \
-      int rc = SET_PORT(pPort, pValue); \
-      if (rc == PORT_NOT_OUTPUT) { \
-         WARNING("Pin "#pPort" is not defined as output", CAT_TIMER, WARN_TIMERS_OUTPUT); \
-      } else if (rc == PORT_NOT_OWNER) { \
-         WARNING("Pin "#pPort" already in use by another peripheral", CAT_TIMER, WARN_TIMERS_OUTPUT); \
-      } \
-   }
+// Size of temporary string buffer for generating filenames and error messages
+#define MAXBUF 256
 
 // On_notify() codes; shared by all peripheral modules. Be careful about inserting/removing
 // enums since the remaining ones are renumbered and this can cause compatibility issues
 // when some DLL files are not recompiled.
+//
 enum {NTF_PRR0, NTF_PRR1, NTF_TSM, NTF_PSR, NTF_WDR};
 
+char *hex(const WORD8 &pData)
+//*******************
 // Return a hex string representation of a WORD8 value. If the WORD8 contains any unknown
 // bits then return "$??". Because this function uses a static buffer, the returned string
 // is only valid until the next call to this function.
-char *hex(const WORD8 &pData)
 {
    static char strBuffer[8];
 
@@ -42,4 +24,18 @@ char *hex(const WORD8 &pData)
       snprintf(strBuffer, 8, "$??");
    
    return strBuffer;
+}
+
+void SetWindowTextf(HWND pHandle, const char *pFormat, ...)
+//*************************
+// Wrapper around SetWindowText() that provides printf() like functionality
+{
+   char strBuffer[MAXBUF];
+   va_list argList;
+
+   va_start(argList, pFormat);
+   vsnprintf(strBuffer, MAXBUF, pFormat, argList);
+   va_end(argList);
+
+   SetWindowText(pHandle, strBuffer);
 }
