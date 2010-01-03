ac6749506e895002b2084275386a4d9653c1a325
thvortex <thvortex@52a0f2d4-074f-11de-b753-7319d013a8fa>
2010-01-03 05:15:59 +0000

    Tag v0.2 of mculib (TIMER2 support added)

---
+++ tags/mculib/mculib-0.2/timer2_168.cpp
@@ -0,0 +1,37 @@
+// =============================================================================
+// Component Name: ATMega168 "TIMER2" peripheral.
+//
+// Version History:
+// v0.1 05/24/09 - Initial public release by AMcTools
+// v1.0 09/15/09 - Fully implemented model
+//
+// Copyright (C) 2009 Advanced MicroControllers Tools (http://www.amctools.com/)
+// Copyright (C) 2009 Wojciech Stryjewski <thvortex@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+#include <windows.h>
+#include <commctrl.h>
+#pragma hdrstop
+#include <stdio.h>
+
+#define IS_PERIPHERAL       // To distinguish from a normal user component
+#include <blackbox.h>
+#include "useravr.h"
+
+#include "timer2_168.h"
+#define TIMER_2
+
+#include "timer_168.cpp"
