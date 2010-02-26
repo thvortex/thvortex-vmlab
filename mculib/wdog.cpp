1436b29dd96a68069d2f5bc91c541c2b89e67ca8
thvortex <thvortex@52a0f2d4-074f-11de-b753-7319d013a8fa>
2010-02-26 04:18:04 +0000

    Watchdog component finished

--- mculib/wdog.cpp
+++ mculib/wdog.cpp
@@ -18,8 +18,13 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //
 
+// NOTE: Because the VMLAB API does not have a "pTime" argument in
+// On_register_write(), it's necessary to keep track of the explicit prescaler
+// counter value and to only use REMIND_ME() with a fixed 128kHz period.
+
 #include <windows.h>
 #include <commctrl.h>
+#include <stdio.h>
 #pragma hdrstop
 
 #define IS_PERIPHERAL       // To distinguish from a normal user component
@@ -29,6 +34,21 @@
 
 int WINAPI DllEntryPoint(HINSTANCE, unsigned long, void*) {return 1;} // is DLL
 // =============================================================================
+
+// Maximum value allowed in WDP field
+#define MAX_PRESCALER_INDEX 9
+
+// Period of one watchdog timer tick of 128kHz clock
+#define WDOG_PERIOD (1.0 / 128000)
+
+// Watchdog mode as a combination of WDE and WDIE bits
+const char *Mode_text[] = {
+   "?", "Disabled", "Interrupt", "Reset", "Interrupt and Reset"
+};
+const char *Prescaler_text[] = {
+   "?", "2K", "4K", "8K", "16K", "32K", "64K", "128K", "256K", "512K", "1024K",
+   "Reserved", "Reserved", "Reserved", "Reserved", "Reserved", "Reserved"
+};
 
 // Involved ports. Keep same order as in .INI file "Port_map = ..." who
 // does the actual assignment to micro ports PD0, etc. This allows multiple instances
@@ -58,20 +78,24 @@ END_INTERRUPTS
 // Wrapper macro to call WARNING() since watchdog has only one category
 #define Warn(text) WARNING(text, CAT_WATCHDOG, WARN_WATCHDOG_SUSPICIOUS_USE)
 
-// Action codes to be used with REMIND_ME2() -> On_remind_me(),
-enum { RMD_AUTOCLEAR_WDCE, RMD_TIMEOUT };
+// Action codes used with REMIND_ME() -> On_remind_me() (must tbe even ints)
+enum { RMD_AUTOCLEAR_WDCE };
 
 // =============================================================================
 // Declare variables here to allow multiple instances to be placed
 //
-DECLARE_VAR
-   int Tick_signature;    // For REMIND_ME2, to validate ticks
-   
+DECLARE_VAR   
    bool Wdton;            // True if the WDTON fuse if programmed (set to 0)
    bool Wdrf;             // True if the WDRF flag is set to 1 in MCUSR
    int Mask;              // Register mask for known/unknown bits
+
+   UINT Count;            // Current prescaler counter value
+   int Prescaler;         // Prescaler divisor selected by WDP bits
+   int Tick_signature;    // For REMIND_ME, to validate ticks
    
    bool Log;              // True if the "Log" checkbox button is checked
+   bool Dirty;            // True if "Mode" and "Clock" fields need update
+   bool Dirty_time;       // True if "Time Left" field needs update
 END_VAR
 
 USE_WINDOW(WINDOW_USER_1); // Window to display registers, etc. See .RC file
@@ -88,19 +112,15 @@ END_VIEW
 // Extract the Watchdog Timer Prescaler (WDP) bits and return as single field
 int Wdp(const WORD8 &pData)
 {
-   return ((pData & 0x5) | ((pData & 0x20) >> 2)).get_field(3, 0);
+   return ((pData & 0x7) | ((pData & 0x20) >> 2)).get_field(3, 0);
 }
 
 //*************************
-// Extract the Watchdog Timer Prescaler (WDP) bits and return as single field
-#if 0
-int Wdp(const WORD8 &pData)
+// Extract the WDE and WDIE bits as a single field
+int Mode(const WORD8 &pData)
 {
-   int low = pData.get_field(2, 0);
-   int high = pData[5];
-   return (low == -1 | high == -1) ? (-1) : ((high << 3) | low);
+   return (((pData & 0x8) >> 2) | ((pData & 0x40) >> 6)).get_field(1, 0);
 }
-#endif
 
 void Log(const char *pFormat, ...)
 //*************************
@@ -150,6 +170,54 @@ void Log_register_write(int pId, WORD8 pData, unsigned char pMask)
    }
 }
 
+void Go()
+//*************************
+// Schedule the next watchdog timer tick
+{
+   VAR(Tick_signature) += 2;            
+   REMIND_ME(WDOG_PERIOD, VAR(Tick_signature));
+}
+
+void Count()
+//*************************
+// Called on every tick of the watchdog timer if the watchdog is enabled
+// (i.e. WDE/WDIE are both known and at least one is true). Increments the
+// prescaler counter, checks counter against the prescaler divisor for
+// a watchdog timeout, and schedules a new watchdog timer tick.
+{
+   // Increment the prescaler counter. Real counter is only 20 bits but for
+   // the modulo arithmetic it doesn't matter that a 32 bit integer is used.
+   // The "Time_left" field will also need updating as a result.
+   VAR(Count)++;
+   VAR(Dirty_time) = true;
+
+   // Schedule next timer tick to keep watchdog running. If watchdog becomes
+   // disabled, then On_register_write() will invalide the Tick_signature.
+   Go();
+
+   // If the prescaler divisor is valid (no unknown bits and not a reserved
+   // value), then compare it against the accumulated ticks and either shedule
+   // an interrupt or do a system reset based on the current watchdog mode.
+   if(VAR(Prescaler) && VAR(Count) % VAR(Prescaler) == 0) {
+   
+      // If WDIE=1 then using either Interrupt or Interrupt/Reset mode
+      if(REG(WDTCSR)[6] == 1) {
+         SET_INTERRUPT_FLAG(WDT, FLAG_SET);
+         REG(WDTCSR).set_bit(7, 1);
+      }
+      
+      // Otherwise WDE=1 should be set for Reset only mode
+      else if(REG(WDTCSR)[3] == 1) {
+         RESET(RESET_WATCHDOG);
+      }
+      
+      // Should never happen; unknown/zero mode should have invalidated tick
+      else {
+         BREAK("Internal WDOG error in Count()");
+      }
+   }
+}
+
 // =============================================================================
 // Callback functions. These functions are called by VMLAB at the proper time
 
@@ -162,21 +230,51 @@ void On_destroy()                         //     "        "
 }
 
 void On_simulation_begin()                //     "        "
+//**********************
+// Called at the beginning of simulation. The rest of initialization
+// happens inside On_reset() since that will always get called immediately
+// after On_simulation_begin().
 {
    // Save the state of the WDTON fuse which locks the WDE and WDIE bits
    // to respectively 1 and 0, forcing the watchdog into system reset mode.
    VAR(Wdton) = GET_FUSE("WDTON") == 0;
+   if(VAR(Wdton)) {
+      PRINT("Watchdog always on (fuse WDTON=0)");
+      VAR(Mask) = 0xB7; // All bits except WDE & WDIE writable
+   } else {
+      VAR(Mask) = 0xFF; // All bits writable
+   }
+
+   // The Tick_signature is always incremented by 2 and therefore it will
+   // always be an odd number. This allows even values in pAux to specify
+   // alternate functions. Note that the Tick_signature is not invalidated
+   // after a reset because the watchdog timer runs independently.
+   VAR(Tick_signature) = 1;      
+
+   // The count is not zeroed on a reset because watchdog keeps runnning
+   VAR(Count) = 0;   
 }
 
 void On_simulation_end()
 //**********************
+// If the simulation is ending, set all registers and text fieds to unknown.
 {
    REG(WDTCSR) = WORD8(0, 0);
-   // TODO: Set Last_WDTCSR equal to the same value
+   SetWindowText(GET_HANDLE(GDT_MODE), "?");
+   SetWindowText(GET_HANDLE(GDT_CLOCK), "?");
+   
+   // Prevent On_update_tick() from changing mode/clock text field but
+   // ensure the "Time Left" field is changed to "? ms".
+   VAR(Prescaler) = 0;
+   VAR(Dirty) = false;
+   VAR(Dirty_time) = true;
+   
+   // WDRF bit in MCUSR is always 0 after a power-on reset
+   VAR(Wdrf) = false;
 }
 
 void On_register_write(REGISTER_ID pId, WORD8 pData)
-//**************************************************
+//**********************
 // The micro is writing pData into the pId register This notification
 // allows to perform all the derived operations.
 // Register selection macros include the "case", X bits check and logging
@@ -184,18 +282,36 @@ void On_register_write(REGISTER_ID pId, WORD8 pData)
    switch(pId) {
       case WDTCSR:
          Log_register_write(WDTCSR, pData, VAR(Mask));
-         
+
+         int oldMode = Mode(REG(WDTCSR));
+
          // Bits 0-2,5 - WDP: Watchdog Timer Prescaler
-         // ----------------------------------------
+         // ----------------------------------------         
          int oldWdp = Wdp(REG(WDTCSR));
          int newWdp = Wdp(pData);
-         
+
          // Changing WDP bits only allowed if WDCE already set
          if(newWdp != oldWdp) {
             if(REG(WDTCSR)[4] != 1) {
                Warn("Cannot change WDP bits if WDCE is not already set");
             } else {
                REG(WDTCSR) = (REG(WDTCSR) & ~0x27) | pData & 0x27;
+               Log("Update prescaler: %s", Prescaler_text[newWdp + 1]);
+
+               if(newWdp > MAX_PRESCALER_INDEX) {
+                  Warn("Reserved WDP value written to WDTCSR");
+               }
+               
+               // Decode WDP field to prescaler divisor
+               if(newWdp >= 0 && newWdp <= MAX_PRESCALER_INDEX) {
+                  VAR(Prescaler) = 0x800 << newWdp;
+               } else {
+                  VAR(Prescaler) = 0;
+               }
+               
+               // Both "Time Left" field and "Clock" field need update
+               VAR(Dirty_time) = true;
+               VAR(Dirty) = true;
             }
          }
          
@@ -214,14 +330,16 @@ void On_register_write(REGISTER_ID pId, WORD8 pData)
             }
          }
          
-         // Writing WDE=X silently ignored if WDTON programmed
+         // If WDTON=0 (programmed) then WDE is read only and always 1
+         // If WDTON=1 then writing WDE=X or WDE=1 always allowed
          else if(!VAR(Wdton)) { 
             REG(WDTCSR).set_bit(3, pData[3]);
          }
          
          // Bit 4 - WDCE: Watchdog Change Enable
          // ----------------------------------------
-         // Writing WDCE=1 only allowed if also writing WDE=1
+         // Writing WDCE=1 only allowed if also writing WDE=1 or if WDCE
+         // was already set to 1 in WDTCSR.
          if(pData[4] == 1 && REG(WDTCSR)[4] != 1) {
             if(pData[3] == 1) {
                REG(WDTCSR).set_bit(4, 1);
@@ -231,11 +349,16 @@ void On_register_write(REGISTER_ID pId, WORD8 pData)
             }         
          }
          
-         // Writing WDCE=0/X is always allowed
-         else {
-            REG(WDTCSR).set_bit(4, pData[4]);
+         // Writing WDCE=X only allowed if WDE=1 or WDE=X
+         else if(pData[4] == UNKNOWN && REG(WDTCSR)[3] != 0) {
+            REG(WDTCSR).set_bit(4, UNKNOWN);
          }
 
+         // Writing WDCE=0 is always allowed
+         else if(pData[4] == 0) {
+            REG(WDTCSR).set_bit(4, 0);
+         }
+         
          // Bit 6 - WDIE: Watchdog Interrupt Enable
          // ----------------------------------------
          // Changes to WDIE bit only allowed if WDTON not programmed
@@ -258,8 +381,23 @@ void On_register_write(REGISTER_ID pId, WORD8 pData)
             SET_INTERRUPT_FLAG(WDT, FLAG_CLEAR);
             REG(WDTCSR).set_bit(7, 0);
          }
+
+         // Log changes made to the timer's mode (WDIE and WDE fields)
+         int newMode = Mode(REG(WDTCSR));
+         if(oldMode != newMode) {
+            Log("Updating mode: %s", Mode_text[newMode + 1]);
+            VAR(Dirty) = true;
+         }
+
+         // If watchdog just became enabled, then schedule new tick
+         if(oldMode <= 0 && newMode > 0) {
+            Go();
+         }
          
-         // TODO: Start timer if WDE=1
+         // If watchdog just became disabled, then cancel pending tick
+         if(oldMode > 0 && newMode <= 0) {
+            VAR(Tick_signature) += 2;
+         }
          
          break;
    }
@@ -268,33 +406,54 @@ void On_register_write(REGISTER_ID pId, WORD8 pData)
 void On_reset(int pCause)
 //***********************
 // Initialize registers to the desired value. Note that the watchdog timer
-// is not automatically disabled nor is the watchdog counter reset.
-{
-   // If WDTON fuse is programmed, then WDE and WDIE forced to 1 and 0
-   if(VAR(Wdton)) {
-      REG(WDTCSR) = 0x08;
-      VAR(Mask) = 0xB7;
-   } else {
-      REG(WDTCSR) = 0;
-      VAR(Mask) = 0xFF;
-   }
-
-   VAR(Tick_signature) = 0;
-   VAR(Wdrf) = false;
+// is not automatically disabled here nor is the watchdog counter reset
+// since the watchdog is independent of the MCU and can continue a
+{   
 
    // The initial state of the WDE bit depends on the reset. If the MCU was
    // reset due to a previous watchdog timeout, then the WDE pin is set to 1
-   // and becomes read-only until the WDRF flag is cleared in MCUSR
+   // and becomes read-only until the WDRF flag is cleared in MCUSR.
    if(pCause == RESET_WATCHDOG) {
       VAR(Wdrf) = true;
-      REG(WDTCSR).set_bit(3, 1);
+   }   
+   
+   // If fuse WDTON=0 (programmed) or WRDF=1 in MCUSR (either from this
+   // RESET_WATCHDOG, from a previous RESET_WATCHDOG, or because AVR code
+   // previously set WRDF=1 manually) then WDE=1 after reset.
+   int oldMode = Mode(REG(WDTCSR));
+   if(VAR(Wdton) || VAR(Wdrf)) {
+      REG(WDTCSR) = 0x08;
+   } else {
+      REG(WDTCSR) = 0;
    }
+   int newMode = Mode(REG(WDTCSR));
+   
+   // Initial WDP value in MCUSR corresponds to 2K prescaler
+   VAR(Prescaler) = 2048;
+      
+   // If the watchdog not already running (either WDTON=0 and this is the
+   // power on reset or because user initiated watchdog reset from GUI)
+   // then schedule the first watchdog timer tick.
+   if(oldMode <= 0 && newMode > 0) {
+      Go();
+   }
+   
+   // If watchdog was already running and became disabled after reset
+   // (e.g. WDIE was 1 before an external reset) then invalidate pending
+   // timer tick.
+   if(oldMode > 0 && newMode <= 0) {
+      VAR(Tick_signature) += 2;
+   }
+
+   // Force the GUI to display new values
+   VAR(Dirty) = true;
+   VAR(Dirty_time) = true;
 }
 
 void On_remind_me(double pTime, int pAux)
-//**************************************
-// Response to REMIND_ME2() used implement the watchdog timeout and the
-// autoclearing of the WDCE bit 4 system clock cycles after it was set
+//***************************************
+// Response to REMIND_ME() used implement the watchdog timeer ticks and the
+// autoclearing of the WDCE bit 4 system clock cycles after it was set.
 {
    switch(pAux) {
       case RMD_AUTOCLEAR_WDCE:
@@ -304,13 +463,16 @@ void On_remind_me(double pTime, int pAux)
          REG(WDTCSR).set_bit(4, 0);
          break;
          
-      case RMD_TIMEOUT:
+      default:
+         if(pAux == VAR(Tick_signature)) {
+            Count();
+         }
          break;
    }
 }
 
 void On_notify(int pWhat)
-//**********************
+//***********************
 // Notification coming from some other DLL instance. Used here to handle
 // the WDR instruction and to handle the clearing of the WDRF bit in MCUSR
 // which allows clearing of the WDE bit.
@@ -319,6 +481,8 @@ void On_notify(int pWhat)
    
       // The WDR instruction was executed; reset watchdog counter
       case NTF_WDR:
+         VAR(Count) = 0;
+         VAR(Dirty_time) = true;
          break;
 
       // The WDRF bit in MCUSR was cleared; WDE is read-write again
@@ -327,10 +491,14 @@ void On_notify(int pWhat)
          break;
          
       // The WDRF bit in MCUSR was set; WDE is set and becomes read-only
+      // If the watchdog was previously disabled, then schedule new tick
       case NTF_WDRF1:
          VAR(Wdrf) = true;
+         if(Mode(REG(WDTCSR)) <= 0) {
+            Go();
+         }
          REG(WDTCSR).set_bit(3, 1);
-         // TODO: Start timer
+         VAR(Dirty) = true;
          break;
    }
 }
@@ -345,10 +513,61 @@ void On_gadget_notify(GADGET pGadget, int pCode)
 }
 
 void On_update_tick(double pTime)
-//************************************************
+//*******************************
 // Called periodically to refresh static controls showing the status, etc.
 // If the watchdog timer is enabled, the "Timeout" field is updated to show
 // remaining time based on the current "pTime". The rest of the GUI is only
 // updated if the WDTCSR register has changed since the last On_update_tick().
 {
+   // Update "Time Left" If either prescaler divisor or count changed
+   if(VAR(Dirty_time)) {
+   
+      // If the prescaler divisor is known, update the "Time Left" based on the
+      // the current counter value and the selected divisor. If the divisor is
+      // unknown then only display "? ms".
+      if(VAR(Prescaler)) {
+         int cycles = VAR(Prescaler) - VAR(Count) % VAR(Prescaler);
+         SetWindowTextf(GET_HANDLE(GDT_TIME), "%.0f ms", cycles * WDOG_PERIOD * 1000);
+      } else {
+         SetWindowText(GET_HANDLE(GDT_TIME), "? ms");
+      }
+      
+      VAR(Dirty_time) = false;
+   }
+
+   // Only upate Mode and Clock fields if WDTCSR changed since last update
+   if(VAR(Dirty)) {
+      int mode = Mode(REG(WDTCSR));
+      
+      // Update mode display as a function of WDE and WDIE
+      SetWindowText(GET_HANDLE(GDT_MODE), Mode_text[mode + 1]);
+      
+      // Update clock display; watchdog timer disabled if WDE=0 and WDIE=0
+      SetWindowTextf(GET_HANDLE(GDT_CLOCK),
+         (mode == 0) ? "Disabled" : "128kHz / %s",
+         Prescaler_text[Wdp(REG(WDTCSR)) + 1]);
+         
+      VAR(Dirty) = false;
+   }
+}
+
+void On_interrupt_start(INTERRUPT_ID pId)
+//***********************************
+// Called when MCU begins to execute the watchdog interrupt. Clears the
+// interrupt flag (WDIF) and the interrupt enable (WDIE) flag to switch the
+// watchdog into reset only mode.
+{
+   switch(pId) {
+      case WDT:
+         // Acknowledge the interrupt by clearing the interrupt flag
+         REG(WDTCSR).set_bit(7, 0);
+         
+         // If WDE=1 (Interrupt and Reset), then set WDIE=0 (Reset only)
+         if(REG(WDTCSR)[3] == 1) {
+            REG(WDTCSR).set_bit(6, 0);
+            VAR(Dirty) = true;
+         }
+         
+         break;
+   }
 }
