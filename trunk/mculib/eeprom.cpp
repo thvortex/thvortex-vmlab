6f8eb6498c57d0c0165eb4dc0370896620dcf028
thvortex <thvortex@52a0f2d4-074f-11de-b753-7319d013a8fa>
2010-03-29 02:12:50 +0000

    Implemented EERE bit for EEPROM reading

--- trunk/mculib/eeprom.cpp
+++ trunk/mculib/eeprom.cpp
@@ -76,7 +76,7 @@ DECLARE_VAR
    UCHAR EEARH_mask;    // Valid bits in EEARH (Version high byte)
    UCHAR EEARL_mask;    // Valid bits in EEARL (Version 2nd highest byte)
    UCHAR EECR_mask;     // Valid bits mask in EECR (Version low byte)
-   int Version;         // Lowest byte of VERSION() that defines EECR
+   UINT Version;        // Lowest byte of VERSION() that defines EECR
 
    bool Log;            // True if the "Log" checkbox button is checked
    bool Delay;          // True if the "Simulate write/erase time" is checked
@@ -182,11 +182,11 @@ const char *On_create()
    // Bitmask for EECR corresponds to the bits defined in REGISTERS_VIEW
    VAR(Version) = VERSION() & 0xFF;
    if(VAR(Version) == 3) {
-      VAR(EECR_mask) = 0x3F;
+      VAR(EECR_mask) = 0x3F; // EEPM1, EEPM0, EERIE, EEMPE, EEPE, EERE
    } else if(VAR(Version) == 2) {
-      VAR(EECR_mask) = 0x0F;
+      VAR(EECR_mask) = 0x0F; // EERIE, EEMWE, EEWE, EERE
    } else if(VAR(Version) == 1) {
-      VAR(EECR_mask) = 0x07;
+      VAR(EECR_mask) = 0x07; // EEMWE, EEWE, EEWE
    } else {
       VAR(EECR_mask) = 0;
    }
@@ -210,8 +210,19 @@ void On_simulation_begin()
 // Called at the beginning of simulation.
 {
    // Ensure that EEPE=0 and no EEPROM erase/write is in progress. On_reset()
-   // will initialize other bits and registers, but will presere EEPE.
+   // will initialize other bits and registers, but will preserve EEPE.
    REG(EECR) = 0;
+   
+   // VLMAB internally loads data from project's .eep file at the beginning
+   // of the simulation. Copy this data to the local memory buffer so that it
+   // can be used with an external hex editor control.
+   for(int i = 0; i < VAR(Size); i++) {
+      WORD8 *data = GET_MICRO_DATA(DATA_EEPROM, i);
+      if(data == NULL) {
+         BREAK("Internal error; GET_MICRO_DATA(DATA_EEPROM) returned NULL");
+      }
+      VAR(Memory)[i] = data->d();
+   }
 }
 
 void On_simulation_end()
@@ -222,9 +233,17 @@ void On_simulation_end()
       REG(i) = WORD8(0,0);
    }
 
-   // TODO: Copy our VAR(Memory) contents back to VMLAB so VMLAB can save the
-   // modified EEPROM back to .eep file. This does not appear to be supported
-   // by VMLAB at the moment.
+   // Copy the local memory buffer back to VMLAB so that any modifications can
+   // be automatically saved back to the project's .eep file.
+   // TODO: This does not appear to be supported by VMLAB at the moment.
+   // TODO: Check "Auto save" option before doing this
+   for(int i = 0; i < VAR(Size); i++) {
+      WORD8 *data = GET_MICRO_DATA(DATA_EEPROM, i);
+      if(data == NULL) {
+         BREAK("Internal error; GET_MICRO_DATA(DATA_EEPROM) returned NULL");
+      }
+      *data = VAR(Memory)[i];
+   }
 }
 
 void On_register_write(REGISTER_ID pId, WORD8 pData)
@@ -232,19 +251,95 @@ void On_register_write(REGISTER_ID pId, WORD8 pData)
 // The micro is writing pData into the pId register. This notification
 // allows to perform all the derived operations.
 {
+   // Bitmask applied to register assignment (all bits R/W by default)
+   UCHAR mask;
+   
+   switch(pId) {
+         
+      // EEPROM high/low address byte
+      //---------------------------------------------------------
+      // Allow changing only those bits defined by VERSION() masks. If
+      // EECR{EEPE]=1 (EEPROM programming in progress) then don't allow
+      // the address registers to be modified.
+      case EEARH:
+      case EEARL:
+         mask = (pId == EEARH) ? VAR(EEARH_mask) : VAR(EEARL_mask);      
+         Log_register_write(pId, pData, mask);
+         
+         if(REG(EECR)[1] == 1) {
+            WARNING("Cannot write EEAR registers while EEPE=1 (EEPROM busy)",
+               CAT_EEPROM, WARN_WRITE_BUSY);
+            mask = 0; // Inhibit register assignment
+         }
+         
+         break;
+      
+      // EEPROM data register
+      //---------------------------------------------------------
+      // No special processing required.
+      // TODO: Can EEDR be modified while write/erase in progress?
+      case EEDR:
+         mask = 0xFF;
+         Log_register_write(EEDR, pData, mask);
+         break;
+      
+      // EEPROM control register
+      //---------------------------------------------------------
+      case EECR:
+         mask = VAR(EECR_mask);
+         Log_register_write(EECR, pData, mask);
+         mask &= 0xFE; // EERE is a strobe bit and always reads 0 in register
+         
+         // Bit 0 - EERE: EEPROM Read Enable
+         //---------------------------------------------------------
+         // If EEPROM is not busy then copy byte from memory buffer to EEDR.
+         // TODO: What happens if EERE=1 and EEPE=1 written at the same time?
+         // Does one take precedence over the other?         
+         if(pData[0] == 1) {
+            if(pData[1] == 1) {
+               WARNING("Cannot read and write/erase EEPROM at the same time",
+                  CAT_EEPROM, WARN_EEPROM_SIMULTANEOUS_RW);
+            } else if(REG(EECR)[1] == 1) {
+               WARNING("Cannot read EEPROM if write/erase already in progress",
+                  CAT_EEPROM, WARN_EEPROM_SIMULTANEOUS_RW);
+            } else {
+
+               // Read EEARL/EAARH pair as little-endian 16bit value
+               WORD16LE *addr = (WORD16LE *) &REG(EEARL);
+               if(!addr->known()) {
+                  WARNING("Unknown bits in EEAR registers",
+                     CAT_EEPROM, WARN_EEPROM_ADDRES_OUTSIDE);
+               } else if(addr->d() > VAR(Size)) {
+                  WARNING("Address in EEAR registers out of range",
+                     CAT_EEPROM, WARN_EEPROM_ADDRES_OUTSIDE);
+               } else {
+                  UCHAR data = VAR(Memory)[addr->d()];
+                  REG(EEDR) = data;
+                  Log("Read EEPROM[$%04X]=$%02X", addr->d(), data);
+               }
+            }
+         }
+         
+         break;
+   }
+   
+   REG(pId) = pData & mask;
 }
 
 void On_reset(int pCause)
 //***********************
 // Initialize registers to the desired value.
 {
-   // Initialize all registers to known zero state, but preserve EECR[EEPE]
-   // bit across reset in case erase/write still in progress.
-   LOGIC eepe = REG(EECR)[2];
+   // Initialize all registers to known zero state. If the EECR[EEPE]
+   // bit is set (EEPROM write/erase still in progress) then preserve the
+   // contents of the EEPE, EEPM1, and EEPM0 bits across reset.
+   WORD8 eecr = REG(EECR);
    FOREACH_REGISTER(i){
       REG(i) = 0;
    }
-   REG(EECR).set_bit(2, eepe);
+   if(eecr[1] == 1) {
+      REG(EECR) = eecr & 0x32;
+   }
 
    // Other than for Version 1 (ATtiny22 like), the implemented bits in the
    // EEARH/EEARL registers are set to unknown state after reset.
@@ -269,6 +364,8 @@ void On_sleep(int pMode)
 //*********************
 // The micro has entered in SLEEP mode.
 {
+   // TODO: Verify how EEPROM is affected by sleep modes? What if SLEEP
+   // entered while EEPROM is actively programming
 }
 
 void On_gadget_notify(GADGET pGadget, int pCode)
