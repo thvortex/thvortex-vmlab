2648ab5b39b4b0a33859bd4839efc079a895aebd
thvortex <thvortex@52a0f2d4-074f-11de-b753-7319d013a8fa>
2010-04-17 05:21:47 +0000

    ShineInHex editor working as MDI child in VMLAB

--- mculib/hexfile.h
+++ mculib/hexfile.h
@@ -35,10 +35,9 @@
 
 // These macros check the assertion that a Win32 API call returned succesfully.
 // If the call failed, then this macro will display a message box with detailed
-// error information. This macro also returns from the current function if the
-// assertion has failed.
+// error information.
 #define W32_ASSERT(cond) if((cond) == 0) \
-   { w32_error("W32_ASSERT( "#cond" );", __FILE__, __LINE__); return; }
+   { w32_error("W32_ASSERT( "#cond" );", __FILE__, __LINE__); }
 
 class Hexfile
 //*********
@@ -52,18 +51,24 @@ private:
    static HMODULE Library;    // Loaded ShineInHex.dll library
    static ATOM MDI_class;     // Window class of MDI_child window
 
+   static const char CLASS_NAME[]; // Window class name used only internally
+   
    HWND MDI_child;  // Child window of MDI_client containing HEX_child
    HWND HEX_child;  // The "shineinhex" class window inside MDI_child
    
-   // Arguments saved from init() function
    void *Pointer;   // Data buffer on which all operations will act on
    int Size;        // Total size of data buffer in bytes
    int Offset;      // Value added to the offset display in the hex editor
    
-   // Default window size for newly created MDI_child
-   enum { MDI_WIDTH = 251, MDI_HEIGHT = 251 };
+   char Dummy;      // Dummy data for editor because HEXM_UNSETPOINTER is buggy
+      
+   // Initial size of MDI child window (not client area)
+   enum { INIT_WIDTH = 629, INIT_HEIGHT = 305 };
    
-   // Window messages supported by hex editor control
+   // Minimum allowed size of MDI child window (not client area)
+   enum { MIN_WIDTH = 250, MIN_HEIGHT = 64 };
+
+   // Window messages supported by hex editor control (no .h file available)
    enum {
       HEXM_SETFONT          = WM_USER+100,
       HEXM_SETOFFSETGRADCOL = WM_USER+101,
@@ -91,23 +96,23 @@ private:
    
    static void w32_error(char *pCode, char *pFile, int pLine);
 
-   static LRESULT CALLBACK MDI_proc(HWND, UINT, WPARAM, LPARAM);
-
+   static LRESULT CALLBACK MDI_proc(HWND, UINT, WPARAM, LPARAM);   
+   static void hide(HWND pWindow);
+   
 public:
    Hexfile();
    ~Hexfile();
 
-   void init(HINSTANCE pInstance, HWND pWindow, char *pTitle = "", int pIcon = 0);
-   
+   void init(HINSTANCE pInstance, HWND pWindow, char *pTitle = "", int pIcon = 0);   
    void data(void *pPointer, int pSize, int pOffset = 0);
    
    void erase();
    void load(char *pFile = NULL);
    void save(char *pFile = NULL);
-   void hide();
 
+   void hide();
    void show();      
-   void redraw();
+   void refresh();
 };
 
 // Definitions for all static class varialbes in Hexfile
@@ -115,8 +120,9 @@ HWND Hexfile::VMLAB_window = NULL;
 HWND Hexfile::MDI_client = NULL;
 ATOM Hexfile::MDI_class = NULL;
 HMODULE Hexfile::Library = NULL;
+const char Hexfile::CLASS_NAME[] = "VMLAB Hexfile Editor";
 
-Hexfile::Hexfile()
+ Hexfile::Hexfile()
 //******************************
 // Default constructor allows Hexfile objects to be placed directly in the
 // DECLAVE_VAR block. Also responsible for loading the ShineInHex DLL which
@@ -133,10 +139,12 @@ Hexfile::~Hexfile()
 // Destructor releases resource previously acquired in init(). The last the
 // Hexfile instance to be destroyed also releases global resources.
 {
-   // TODO: Call HEXM_UNSETPOINTER before destroying window
+   // TODO: Call HEXM_UNSETPOINTER before destroying windows?
 
-   // Calling hide() will actually destroy MDI child if it exists
-   hide();
+   // Destroying the MDI_child will also destroy the contained HEX_child.
+   if(MDI_child) {
+      SendMessage(MDI_client, WM_MDIDESTROY, (WPARAM) MDI_child, 0);
+   }
    
    // When the last Hexfile instance is destroyed, then the library's
    // reference count will drop to zero and it will be unloaded.
@@ -156,7 +164,6 @@ void Hexfile::init(HINSTANCE pInstance, HWND pWindow, char *pTitle, int pIcon)
    // TODO: Verify args are not NULL; 
    // TODO: Verity pInstance is same as before
    // TODO: Verify not already called on same window
-   // TODO: Save pInstance somewhere
    
    // First init() finds top-level VMLAB window so it can later be passed as
    // hwndOwner to GetOpenFileName(), GetSaveFileName(), and MessageBox().
@@ -188,10 +195,9 @@ void Hexfile::init(HINSTANCE pInstance, HWND pWindow, char *pTitle, int pIcon)
          pInstance,           // hInstance (DLL instance from DllEntryPoint)
          NULL,                // hIcon (use icon() to set per window icon)
          NULL,                // hCursor (use default cursor) // TODO: use constant
-         //NULL,                // hbrBackground (hex editor fills entire window)
-         (HBRUSH)(COLOR_WINDOW+1), // hbrBackground (TODO: hack to show window)
+         NULL,                // hbrBackground (hex editor fills entire window)
          NULL,                // lpszMenuName (no default class menu needed)
-         "hexfile_c",         // lpszClassName (class name used only in Hexfile)
+         CLASS_NAME,          // lpszClassName (class name used only in Hexfile)
          NULL,                // hIconSm (use icon() to set per window icon)
       };
       W32_ASSERT( MDI_class = RegisterClassEx(&mdiClass) );
@@ -199,58 +205,105 @@ void Hexfile::init(HINSTANCE pInstance, HWND pWindow, char *pTitle, int pIcon)
 
    // Create initially hidden MDI child window containing the hex editor
    // control window as a child. Calling show() will make the window visible
-   // and clicking the window's close box (SC_CLOSE) will hide it again.
-   
-   // TODO: Need to re-create window on each show()
-   // TODO: Use SetWindowPlacement() in show() to restore position of previous
-   // window within the same VMLAB debug session
-   // TODO: Use WM_MDICREATE here instead; no longer need WM_MDIREFRESHMENU
-   // TODO: SetWindowLong() to chnage extended styles to match those in VMLAB
+   // and clicking the window's close box (SC_CLOSE) will hide it again.   
    if(MDI_child == NULL) {
       MDI_child = CreateMDIWindow(
-         "hexfile_c",     // lpClassName (previously used in RegisterClassEx)
+         CLASS_NAME,      // lpClassName (returned by RegisterClassEx)
          pTitle,          // lpWindowName (text displayed in title bar)
-         WS_CHILD |       // dwStyle
-         WS_CLIPSIBLINGS |
-         WS_CLIPCHILDREN |
-         WS_OVERLAPPEDWINDOW |
-         WS_VISIBLE,
+         WS_OVERLAPPEDWINDOW, // dwStyle (same style as other MDI children)
          0,               // X (upper left corner of VMLAB window)
          0,               // Y (upper left corner of VMLAB window)
-         MDI_WIDTH,       // nWidth (default based on Control Panel size)
-         MDI_HEIGHT,      // nHeight (default based on Control Panel size)
+         INIT_WIDTH,      // nWidth (default based on Control Panel size)
+         INIT_HEIGHT,     // nHeight (default based on Control Panel size)
          MDI_client,      // hWndParent (parent MDI client window for child)
          pInstance,       // hInstance (window was created by this DLL)
          NULL             // lParam (no special value to pass to window proc)
       );
-      W32_ASSERT( MDI_child );  
-
-      // Request that the MDI client refreshes the "Windows" menu in the
-      // top-most VMALB window to include the newly created MDI child.
-      SendMessage(MDI_client, WM_MDIREFRESHMENU, 0, 0);
-      W32_ASSERT( DrawMenuBar(VMLAB_window) );
-   }   
+      W32_ASSERT( MDI_child );
+      
+      // TODO: Assert that HEX_child == NULL
+      
+      // Hex editor window alays fills entire client area of MDI child
+      RECT rect;
+      W32_ASSERT( GetClientRect(MDI_child, &rect) );      
+      
+      // Create a hex editor window embedded in MDI child window
+      HEX_child = CreateWindowEx(
+         WS_EX_CLIENTEDGE,// dwExStyle (extra inset border to match VMLAB)
+         "SHINEINHEX",    // lpClassName (registered by loading ShineInHex.dll)
+         "",              // lpWindowName (not dispplayed by hex editor)
+         WS_VISIBLE |     // dwStyle (always visible in parent window)
+         WS_CHILD |       // dwStyle (child window with no frame)
+         WS_VSCROLL,      // dwStyle (show vertical scroll bar)
+         0,               // x (fills entire client area of parent)
+         0,               // y (fills entire client area of parent)
+         rect.right,      // nWidth (fills entire client area of parent)
+         rect.bottom,     // nHeight (fills entire client area of parent)
+         MDI_child,       // hWndParent (control embeds in MDI child window)
+         NULL,            // hMenu (used as id; not needed for one child)
+         pInstance,       // hInstance (window was created by this DLL)
+         NULL             // lParam (no special value to pass to window proc)
+      );
+      W32_ASSERT( HEX_child );
+      
+      // All text in hex edit is displayed in black and all backgrounds are
+      // drawn in white to match the layout of other VMLAB windows. Current
+      // cursor position is shown as white text on a black background, and
+      // selections in the hex editor are white text on a blue background.
+      COLORREF txtColFg = GetSysColor(COLOR_WINDOWTEXT);
+      COLORREF txtColBg = GetSysColor(COLOR_WINDOW);
+      COLORREF selColFg = GetSysColor(COLOR_HIGHLIGHTTEXT);
+      COLORREF selColBg = GetSysColor(COLOR_HIGHLIGHT);
+      COLORREF hdrColFg = GetSysColor(COLOR_BTNTEXT);
+      COLORREF hdrColBg = GetSysColor(COLOR_BTNFACE);
+      
+      // Initialize display settings for ShineInHex control
+      SendMessage(HEX_child, HEXM_SETFONT, 3, 0); // Courier New font
+      SendMessage(HEX_child, HEXM_SETOFFSETGRADCOL, hdrColBg, hdrColBg);
+      SendMessage(HEX_child, HEXM_SETHEADERGRADCOL, hdrColBg, hdrColBg);
+      SendMessage(HEX_child, HEXM_SETVIEW1TEXTCOL, hdrColFg, hdrColFg);
+      SendMessage(HEX_child, HEXM_SETHEADERTEXTCOL, hdrColFg, hdrColFg);
+      SendMessage(HEX_child, HEXM_SETVIEW2COL, txtColBg, txtColFg);
+      SendMessage(HEX_child, HEXM_SETVIEW3COL, txtColBg, txtColFg);
+      SendMessage(HEX_child, HEXM_SETVIEW2SELCOL, selColBg, selColFg);
+      SendMessage(HEX_child, HEXM_SETVIEW3SELCOL, selColBg, selColFg);
+      SendMessage(HEX_child, HEXM_SETACTIVECHARCOL, txtColFg, txtColBg);
+      SendMessage(HEX_child, HEXM_SETMODBYTES1COL, txtColBg, txtColFg);
+      SendMessage(HEX_child, HEXM_SETMODBYTES2COL, txtColBg, txtColFg);
+      SendMessage(HEX_child, HEXM_SETMODBYTES3COL, txtColBg, txtColFg);
+      
+      // The hex editor doesn't work very well without any data and
+      // will in fact crash if you try typing in the edit window. If
+      // show() is called before data(), this will ensure the editor
+      // functions properly until data() is called.
+      SendMessage(HEX_child, HEXM_SETPOINTER, (WPARAM) &Dummy, 1);      
+   }
+   
+   show(); // TODO: DELETE LATER
 }
 
 void Hexfile::data(void *pPointer, int pSize, int pOffset)
 //******************************
 {
+   // TODO: assert that HEX_child should always exist
+
    // Update local variables used by load() and save()
    Pointer = pPointer;
    Size = pSize;
    Offset = pOffset;
    
-   // If hex editor window exists, then update its internal variables
-   // TODO: HEX_child should always exist
-   if(HEX_child) {
-      SendMessage(HEX_child, HEXM_SETOFFSET, (WPARAM) Offset, 0);
-      if(Pointer && Size) {
-         SendMessage(HEX_child, HEXM_SETPOINTER, (WPARAM) Pointer, Size);
-      } else {
-         SendMessage(HEX_child, HEXM_UNSETPOINTER, 0, 0);
-      }
-      // TODO: Possibly invalidate window so it redraws
+   // Update internal state in the editor. Because the editor is not stable
+   // without any data, the Dummy member variable is used as a placeholder
+   // when unsetting the internal data pointers.   
+   SendMessage(HEX_child, HEXM_SETOFFSET, (WPARAM) Offset, 0);
+   if(Pointer && Size) {
+      SendMessage(HEX_child, HEXM_SETPOINTER, (WPARAM) Pointer, Size);
+   } else {
+      SendMessage(HEX_child, HEXM_SETPOINTER, (WPARAM) &Dummy, 1);
    }
+   
+   // Force a redraw of the hex editor window
+   InvalidateRect(HEX_child, NULL, true);
 }
 void Hexfile::hide()
 //******************************
@@ -258,42 +311,127 @@ void Hexfile::hide()
 // calling should not be necessary since closing the window will automatically
 // perform the same action.
 {
-   // Destroying the MDI_child will also destroy the contained HEX_child.
-   if(MDI_child) {
-      SendMessage(MDI_client, WM_MDIDESTROY, (WPARAM) MDI_child, 0);
-   }
+   // TODO: Assert MDI_child window already exists; init() was called
+
+   hide(MDI_child);
+}
+
+void Hexfile::refresh()
+//******************************
+// Update displayed hex data because simulation has changed memory contents
+{
+   // Hex editor seems to use double buffering to prevent flicker. Sending
+   // a HEXM_SETPOINTER forced the editor to re-read the data memory..
+   data(Pointer, Size, Offset);
 }
 
 void Hexfile::show()
 //******************************
 // Called to handle "View" button in the GUI. the MDI child window that was
-// previously created inside init() is made visible and is activated (i.e. is
-// brought to the front and obtains keyboard focus).
+// previously created inside init() is made visible.
 {   
    // TODO: Assert MDI_child window already exists; init() was called
-   
-   // TODO: Hiding the window is not good enough for 
-   
+      
+   // Make window visible and activate (i.e. is brought to the front and
+   // obtains keyboard focus). Request that the MDI client refreshes the
+   // "Windows" menu in the top-most VMALB window to include the now visible
+   // MDI child.
    ShowWindow(MDI_child, SW_SHOW);
-   SendMessage(MDI_client, WM_MDIACTIVATE, (WPARAM) MDI_child, 0);
+   SendMessage(MDI_client, WM_MDIACTIVATE, (WPARAM) MDI_child, 0);   
+   SendMessage(MDI_client, WM_MDIREFRESHMENU, 0, 0);
+   W32_ASSERT( DrawMenuBar(VMLAB_window) );   
+}
+
+void Hexfile::hide(HWND pWindow)
+//******************************
+// This function is called from hide(void) and from the MDI window procedure
+// (on WM_SYSCOMMAND/SC_CLOSE) to perform the real work of hiding the MDI
+// child window.
+{
+   // Hide this MDI child window, active the next MDI child, and request that
+   // the MDI client refreshes the "Windows" menu in the top-most VMALB window
+   // to remove the hidden MDI child.
+   ShowWindow(pWindow, SW_HIDE);
+   SendMessage(MDI_client, WM_MDINEXT, 0, 0);
+   SendMessage(MDI_client, WM_MDIREFRESHMENU, 0, 0);
+   W32_ASSERT( DrawMenuBar(VMLAB_window) );   
 }
 
 LRESULT CALLBACK Hexfile::MDI_proc(HWND pWindow, UINT pMessage, WPARAM pW, LPARAM pL)
 //******************************
 {
+   HWND child;
+
    switch(pMessage) {
-   
       // System menu (close/minimize/maximise) command. Closing the window
       // (SC_CLOSE) will merely hide it instead of destryoing it. All other
       // system commands are passed through.
       case WM_SYSCOMMAND:
+      {
          if(pW == SC_CLOSE) {
-            ShowWindow(pWindow, SW_HIDE);
+            hide(pWindow);
             return true;
          }
          break;
+      }
 
-      // All other messages are processed by DefMDIChildProc()
+      // The user is currently resizing window by dragging its corners. Since
+      // the hex editor control doesn't draw properly at small sizes, a minimum
+      // width has to be enforced here. For consistency, a minimum height is
+      // also enforced which guarantees at least one row of data is visible.
+      case WM_SIZING:
+      {
+         RECT *rect = (RECT *) pL;
+         
+         // Enfore minimum width otherwise
+         if(rect->right - rect->left < MIN_WIDTH) {
+            if(pW == WMSZ_LEFT || pW == WMSZ_TOPLEFT || pW == WMSZ_BOTTOMLEFT) {
+               rect->left = rect->right - MIN_WIDTH;
+            }
+            if(pW == WMSZ_RIGHT || pW == WMSZ_TOPRIGHT || pW == WMSZ_BOTTOMRIGHT) {
+               rect->right = rect->left + MIN_WIDTH;
+            }
+         }
+
+         // Enfore minimum width otherwise
+         if(rect->bottom - rect->top < MIN_HEIGHT) {
+            if(pW == WMSZ_TOP || pW == WMSZ_TOPLEFT || pW == WMSZ_TOPRIGHT) {
+               rect->top = rect->bottom - MIN_HEIGHT;
+            }
+            if(pW == WMSZ_BOTTOM || pW == WMSZ_BOTTOMLEFT || pW == WMSZ_BOTTOMRIGHT) {
+               rect->bottom = rect->top + MIN_HEIGHT;
+            }
+         }
+         
+         break;
+      }
+         
+      // If the MDI window changed size then update hex editor size to
+      // always fill entire client area of MDI window. Note that the first
+      // WM_SIZE passed to MDI_proc is before the HEX_child window exists
+      // yet.
+      case WM_SIZE:
+      {
+         HWND child = GetWindow(pWindow, GW_CHILD);
+         if(child) {
+            W32_ASSERT( MoveWindow(child, 0, 0, LOWORD(pL), HIWORD(pL), true) );
+         }
+         break;
+      }
+      
+      // If MDI client is activated, make sure hex editor has keyboard focus
+      case WM_MDIACTIVATE:
+      {
+         // Only process if this is an activate and not a de-activate event
+         if(pWindow == (HWND) pL) {
+            HWND child = GetWindow(pWindow, GW_CHILD);
+            if(child) {
+               SendMessage(child, WM_SETFOCUS, 0, 0);
+            }
+         }
+         break;
+      }
+
       default:
          break;
    };
