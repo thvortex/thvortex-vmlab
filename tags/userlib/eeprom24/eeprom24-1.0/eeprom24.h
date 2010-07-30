e70e734a9b4d766f8eac70643cbf6702e76bc5f0
thvortex <thvortex@52a0f2d4-074f-11de-b753-7319d013a8fa>
2010-07-30 19:05:09 +0000

    Tag current trunk/eeprom24 as eeprom24-1.0

---
+++ tags/userlib/eeprom24/eeprom24-1.0/eeprom24.h
@@ -0,0 +1,29 @@
+// =============================================================================
+// Component Name: I2C 24xxx series EEPROM
+//
+// Copyright (C) 2009 Wojciech Stryjewski <thvortex@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+
+#define GDT_STATUS  GADGET1
+#define GDT_SLAVE   GADGET2
+#define GDT_ADDR    GADGET3
+#define GDT_BREAK   GADGET4
+#define GDT_LOG     GADGET5
+#define GDT_VIEW    GADGET6
+#define GDT_LOAD    GADGET7
+#define GDT_SAVE    GADGET8
+#define GDT_ERASE   GADGET9
