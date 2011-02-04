29ae07c12e3db15ab36738343f4aa79c60a8d5fe
thvortex <thvortex@52a0f2d4-074f-11de-b753-7319d013a8fa>
2011-02-04 17:01:03 +0000

    Tag mculib-0.6.1 bug fix release

---
+++ mculib-0.6.1/timer2_168.cpp
@@ -0,0 +1,39 @@
+// =============================================================================
+// Component Name: ATMega168 "TIMER2" (asynchronous) peripheral.
+//
+// Copyright (C) 2009 Advanced MicroControllers Tools (http://www.amctools.com/)
+// Copyright (C) 2009, 2010, 2011 Wojciech Stryjewski <thvortex@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+#include <windows.h>
+#include <commctrl.h>
+#pragma hdrstop
+#include <stdio.h>
+
+#define IS_PERIPHERAL       // To distinguish from a normal user component
+#include "blackbox.h"
+#include "useravr.h"
+
+#include "timer2_168.h"
+
+#define TIMER_2
+#define WORDSZ WORD8
+#define TCCRnB_MASK 0xCF
+#define TCCRnB_RW_MASK 0x0F
+#define TIMSKn_MASK 0x07
+#define REGHL(x) REG(x)
+
+#include "timer_168.cpp"
